1bd6ac0eae735426b98f9b00a44845bba6b6f457
Galis <galis.travelling@gmail.com>
2024-08-09 11:38:09 +0200

    fixing config.ts issue

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,8 +7,7 @@ const nodeEnv = process.env.NODE_ENV;
 export const getBackUrl = () => {
   if (nodeEnv === "development") {
     return "http://localhost:4000";
-  }
-  if (nodeEnv === "production") {
+  } else {
     return "https://api-weatherapp-4e636462236d.herokuapp.com";
   }
 };
